5b7305b74d42d47cb765e149a417ffdfa57d9905
Robert Cohn <Robert.S.Cohn@intel.com>
2013-07-07 10:11:45 -0400

    Works locally

--- app.js
+++ app.js
@@ -1,35 +1,100 @@
+var util = require('util');
+var express  = require('express');
 
-/**
- * Module dependencies.
- */
+var config = require('./config');
+var gcal = require('google-calendar');
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+/*
+  ===========================================================================
+            Setup express + passportjs server for authentication
+  ===========================================================================
+*/
 
 var app = express();
+var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var port = process.env.PORT || 5000;
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+});
+app.listen(port);
+console.log('listening on port: ' + port);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+passport.use(new GoogleStrategy({
+    clientID: config.consumer_key,
+    clientSecret: config.consumer_secret,
+    callbackURL: config.callbackURL,
+    scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
+  },
+  function(accessToken, refreshToken, profile, done) {
+    profile.accessToken = accessToken;
+    return done(null, profile);
+  }
+));
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/auth',
+  passport.authenticate('google', { session: false }));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get('/auth/callback', 
+  passport.authenticate('google', { session: false, failureRedirect: '/login' }),
+  function(req, res) { 
+    req.session.access_token = req.user.accessToken;
+    res.redirect('/');
+  });
+
+
+/*
+  ===========================================================================
+                               Google Calendar
+  ===========================================================================
+*/
+
+app.all('/', function(req, res){
+  
+  if(!req.session.access_token) return res.redirect('/auth');
+  
+  //Create an instance from accessToken
+  var accessToken = req.session.access_token;
+  var google_calendar = new gcal.GoogleCalendar(accessToken);
+  
+  google_calendar.calendarList.list(function(err, data) {
+    if(err) return res.send(500,err);
+    return res.send(data);
+  });
+});
+
+app.all('/:calendarId', function(req, res){
+  
+  if(!req.session.access_token) return res.redirect('/auth');
+  
+  //Create an instance from accessToken
+  var accessToken     = req.session.access_token;
+  var google_calendar = new gcal.GoogleCalendar(accessToken);
+  var calendarId      = req.params.calendarId;
+  
+  google_calendar.events.list(calendarId, function(err, data) {
+    if(err) return res.send(500,err);
+    return res.send(data);
+  });
+});
+
+
+app.all('/:calendarId/:eventId', function(req, res){
+  
+  if(!req.session.access_token) return res.redirect('/auth');
+  
+  //Create an instance from accessToken
+  var accessToken     = req.session.access_token;
+  var google_calendar = new gcal.GoogleCalendar(accessToken);
+  var calendarId      = req.params.calendarId;
+  var eventId         = req.params.eventId;
+  
+  google_calendar.events.get(calendarId, eventId, function(err, data) {
+    if(err) return res.send(500,err);
+    return res.send(data);
+  });
 });
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    callbackURL: "http://localhost:5000/auth/callback",
+    //callbackURL: "http://aqueous-peak-9810.herokuapp.com/auth/callback",
+    consumer_key: '768665063514-kg36j8rih2re987eu3t2c5cilugr64uc.apps.googleusercontent.com',
+    consumer_secret: 'zxXge2iXxHtJ2IYzZCBiKB60',
+};
