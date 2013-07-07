a0fdf4b26548cb67212d2c816b53d1fb3ea88b3f
Robert Cohn <Robert.S.Cohn@intel.com>
2013-07-07 19:02:33 -0400

    works

--- routes/mit.js
+++ routes/mit.js
@@ -1,37 +1,10 @@
 var async = require('async');
 var googleCalendar = require('google-calendar');
-var calendarId = "lldb7h5rn85o734jqi4p1i97ns@group.calendar.google.com";
+
+var calendarId = 'ncqr7uo4nbkbd10qsmaj4v9gg8@group.calendar.google.com';
 
 var fetchMIT = require('../lib/fetchMIT');
 
-/*
-exports.json = function(req, res) {
-    fetchMIT(function(data) {
-        res.send(data);
-    });
-}
-
-var updateGcal = function(accessToken, data, cb) {
-    //Create an instance from accessToken
-    var gcal = new gcal.GoogleCalendar(accessToken);
-
-    gcal.events.quickAdd(calendarId, 'some text', function(err, data) {
-        if(err) return cb(err);
-        cb(err);
-    });
-}
-
-exports.update = function(req, res) {
-    if(!req.session.access_token) return res.redirect('/auth');
-
-    fetchMIT(function(data) {
-        updateGcal(req.session.access_token, data, function(err) {
-            if (err) return res.send(500,err);
-            return res.send('OK');
-        });
-    });
-}
-*/
 var list = function(gcal,cb) {
     gcal.events.list(calendarId, {}, cb);
 };
@@ -42,22 +15,47 @@ var listSrc = function(cb) {
     })
 };
 
-var reset = function(gcal,cb) {
+var waitCalEmpty = function(gcal, cb) {
     gcal.events.list(calendarId, {}, function(err, data) {
-        async.each(data.items, 
-                   function(item, cb) {
-                       gcal.events.delete(calendarId, item.id, cb);
-                   }, 
-                   cb);
-    });;
+        if (err)
+            cb(err);
+        console.log('  items left: ' + data.items.length);
+        if (data.items.length == 0) {
+            return cb();
+        }
+        console.log('  scheduling another wait');
+        setTimeout(function() {waitCalEmpty(gcal, cb);}, 10000);
+    });
+}
+
+var reset = function(gcal,cb) {
+    console.log('Reset');
+    gcal.events.list(calendarId, {}, function(err, data) {
+        console.log('  deleting items: ' + data.items.length);
+        // Should be possible to do in parallel, but node gets an error and stops
+        async.eachSeries(data.items, 
+                         function(item, cb) {
+                             gcal.events.delete(calendarId, item.id, cb);
+                         }, 
+                         function(err) {
+                             console.log('  issued delete requests');
+                             if (err)
+                                 return cb(err);
+                             waitCalEmpty(gcal, cb);
+                         });
+    });
 }
 
 var update = function(gcal, cb) {
+    console.log('Update');
     reset(gcal, function(err) {
+        console.log('  reset done');
         if (err) return cb(err);
         fetchMIT(function(data) {
-            //data.events = [data.events[0]];
-            async.each(data.events,
+            console.log('  fetch done');
+            console.log('  inserting events: ' + data.events.length);
+            // Should be possible to do in parallel, but node gets an error and stops
+            async.eachSeries(data.events,
                        function(event, cb) {
                            gcal.events.insert(calendarId, event, cb);
                        },
@@ -70,7 +68,7 @@ module.exports = function(req, res) {
     if (process.env.ACCESS_TOKEN)
         req.session.access_token = process.env.ACCESS_TOKEN;
     if(!req.session.access_token) return res.redirect('/auth');
-    console.log(JSON.stringify(req.session.access_token));
+    //console.log(JSON.stringify(req.session.access_token));
     var gcal = new googleCalendar.GoogleCalendar(req.session.access_token);
     var cb = function(err, data) {
         if (err) return res.send(500,err);
@@ -84,6 +82,7 @@ module.exports = function(req, res) {
     case 'update':
         update(gcal, 
                function(err) {
+                   console.log('  update done');
                    if (err) return cb(err);
                    else list(gcal, cb);
                });
